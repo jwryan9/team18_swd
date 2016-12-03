e3167e7c2bfda061f17dae9b27c9036dcda66a85
Jason Ryan <jason-ryan@uiowa.edu>
2016-12-02 20:31:10 -0600

    Added JavaDoc comments

--- VoterRegistration/src/RunEncryptor.java
+++ VoterRegistration/src/RunEncryptor.java
@@ -2,9 +2,7 @@ import java.io.*;
 import java.util.Scanner;
 
 /**
- * @author Adam Gary
- * @version 1.0
- *
+ * Class for encrypting strings
  */
 public class RunEncryptor {
     /**
--- VoterRegistration/src/Voter.java
+++ VoterRegistration/src/Voter.java
@@ -1,28 +1,59 @@
 /**
- * Created by AdamGary on 12/1/16.
+ * Class for representing voters
  */
 public class Voter {
 
-
+    /**
+     * Voter id for database
+     */
     private String ID;
+
+    /**
+     * Voter's name
+     */
     private String name;
+
+    /**
+     * Voter's zip code
+     */
     private String zipCode;
 
-
+    /**
+     * Constructor for creating voter objects
+     *
+     * @param encryptedID encrypted id for securely referencing voter information
+     * @param name name of voter
+     * @param zipCode zip code of voter's residence
+     */
     public Voter(String encryptedID, String name, String zipCode){
         this.ID = encryptedID;
         this.name = name;
         this.zipCode = zipCode;
     }
 
+    /**
+     * Getter method for voter id
+     *
+     * @return encrypted voter id
+     */
     public String getID() {
         return ID;
     }
 
+    /**
+     * Getter method for voter name
+     *
+     * @return voter name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Getter method for voter zip code
+     *
+     * @return voter zip code
+     */
     public String getZipCode() {
         return zipCode;
     }
